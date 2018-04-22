3a1932e94aa72e2b4c972e83565bce6c8261f271
Hawking <bonieck.douglas@hotmail.com>
2018-04-22 17:28:14 -0300

    1.0

---
+++ _css/index.php
@@ -0,0 +1,14 @@
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?<?php 
+	while ( <= 10) {
+		# code...
+	}
+		 ?>
+</body>
+</html>
