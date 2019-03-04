f0d163abc19fd448b9c217021ba1b4c53dd1cf16
Bonieck Douglas <bonieck.douglas@hotmail.com>
2019-03-04 17:04:16 -0300

    Layout de Vídeo

--- _javascript/script.js
+++ _javascript/script.js
@@ -4,4 +4,18 @@ var bleep = new Audio();
 
                     var tleep = new Audio();
                     tleep.src = "_sons/hover1.ogg";
-                    ;
+                    ;                   
+
+                    function mudaFoto (foto){
+                        document.getElementById("icon").src =foto;
+                    }
+
+                    $(".diamond-grid").diamonds({
+                        size : 220, // Size of diamonds in pixels. Both width and height. 
+                        gap : 5, // Pixels between each square.
+                        hideIncompleteRow : false, // Hide last row if there are not enough items to fill it completely.
+                        autoRedraw : true, // Auto redraw diamonds when it detects resizing.
+                        itemSelector : ".item" // the css selector to use to select diamonds-items.
+                });	 
+
+             
